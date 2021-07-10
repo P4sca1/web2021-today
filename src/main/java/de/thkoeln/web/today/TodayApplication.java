f72a0f9d82ca91444ec260646e506617045111fb
Pascal Sthamer <sthamer.pascal@gmail.com>
2021-07-10 14:34:47 +0200

    Prepare code for tomcat deploy

--- src/main/java/de/thkoeln/web/today/TodayApplication.java
+++ src/main/java/de/thkoeln/web/today/TodayApplication.java
@@ -2,11 +2,18 @@ package de.thkoeln.web.today;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(Config.class)
-public class TodayApplication {
+public class TodayApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodayApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodayApplication.class, args);
